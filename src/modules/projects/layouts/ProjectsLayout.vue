065fad37b443566557ab658d0af38227950b9727
Marcos Lora <marcoslora95@gmail.com>
2024-07-20 18:41:48 -0400

    starting the project

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,14 @@
+import router from '../../../router/index';
+<template>
+  <div class="fle flex-col">
+    <!-- Top Menu -->
+    <TopMenu />
+    <main>
+      <!-- Side Menu -->
+      <router-view />
+    </main>
+  </div>
+</template>
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+</script>
